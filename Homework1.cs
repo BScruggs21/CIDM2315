b46f72a2dce62ba9512929ebf2af9e61d8b742c6
BScruggs21 <157432937+BScruggs21@users.noreply.github.com>
2024-01-22 19:18:00 -0600

    Add files via upload

---
+++ Homework1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double X = 2.5;
+        double Y = 3.3;
+        double Z = 4 * Math.Pow(X, 2) + 3 * Y;
+        Console.WriteLine($"The value of Z for X = {X} and Y = {Y} is: {Z}");
+    }
+}
